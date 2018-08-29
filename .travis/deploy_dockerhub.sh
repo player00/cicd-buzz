d575c3ac7a7f6afd4fa5757c4ad2ae3dcfe41851
Sergei Shalygin <sergei.shalygin@t-systems.ru>
2018-08-29 15:18:58 +0300

    docker login fix

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWORD
+docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     $TAG="latest"
 else
